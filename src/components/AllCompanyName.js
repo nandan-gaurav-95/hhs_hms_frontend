e798238307bda853d24d6fbf8a0cd347570f9e03
gaurav <gaurav.nandan09@gmail.com>
2023-09-18 18:30:27 +0530

    Search added

--- src/components/AllCompanyName.js
+++ src/components/AllCompanyName.js
@@ -2,25 +2,26 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { APIS } from "./constants/api";
 import { useNavigate } from "react-router-dom";
+import { FaSearch, FaMicrophone } from "react-icons/fa"; 
 
 import {
   MDBContainer as Container,
   MDBRow as Row,
   MDBCol as Col,
-  // MDBInput as Input,
   MDBBtn as Button,
 } from "mdb-react-ui-kit";
 
 const AllCompanyName = () => {
   const [allCompany, setAllCompany] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
+
   useEffect(() => {
     async function fetchCompanyNames() {
       try {
         const response = await axios.get(APIS.ALLCOMPANYNAME);
-        console.log("Hiiiii",response.data);
         if (response.status === 200) {
-          setAllCompany(response.data); // Assuming the response contains the list of company names
+          setAllCompany(response.data);
         } else {
           console.error("Error while fetching company names");
         }
@@ -34,27 +35,64 @@ const AllCompanyName = () => {
   const handleViewDetails = async (id) => {
     navigate(`/comapany-details/${id}`);
   };
+  const handleSearch = () => {
+    console.log("Performing search for:", searchQuery);
+  };
+
+  const handleVoiceSearch = () => {
+    console.log("Initiating voice search...");
+  };
 
   return (
-    <div className=" p-5 mt-5 ">
-      <h2 className="text-center mb-4">Company Names:</h2>
+    <div className="p-5 mt-5 text-center">
+      <h2 className="mb-4">Company Names:</h2>
+      <Col className="mb-4 d-flex flex-column align-items-center">
+        <div className="input-group" style={{ maxWidth: "300px" }}>
+          <input
+            type="text"
+            placeholder="Search Company..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="form-control rounded"
+            style={{ borderTopRightRadius: "1.25rem", borderBottomRightRadius: "1.25rem" }}
+          />
+          <div className="input-group-append">
+            <span className="input-group-text" onClick={handleSearch}>
+              <FaSearch />
+            </span>
+          </div>
+          <div className="input-group-append">
+            <span className="input-group-text" onClick={handleVoiceSearch}>
+              <FaMicrophone />
+            </span>
+          </div>
+        </div>
+        
+      </Col>
       <Row className="justify-content-center">
-        <Col className="col-sm-5">
+        <Col className="col-sm-5 d-flex justify-content-center">
           <ul className="list-group">
-            {allCompany.map((company, index) => (
-              <li
-                key={index}
-                className="list-group-item d-flex justify-content-between align-items-center"
-              >
-                {company?.companyNm}
-                <Button
-                  color="primary"
-                  onClick={() => handleViewDetails(company.id)}
+            {allCompany
+              .filter((company) =>
+                company?.companyNm
+                  .toLowerCase()
+                  .includes(searchQuery.toLowerCase())
+              )
+              .map((company, index) => (
+                <li
+                  key={index}
+                  className="list-group-item d-flex justify-content-between align-items-center"
                 >
-                  View Property Details
-                </Button>
-              </li>
-            ))}
+                  {company?.companyNm}
+                  <Button
+                    color="primary"
+                    onClick={() => handleViewDetails(company.id)}
+                    
+                  >
+                    View Property Details
+                  </Button>
+                </li>
+              ))}
           </ul>
         </Col>
       </Row>
