cca5d86467decd5eabe06f3a6cd0c59b73e78b9d
gaurav <gaurav.nandan09@gmail.com>
2023-09-23 15:55:06 +0530

    settiing the git

--- src/components/daybook/DayBook.js
+++ src/components/daybook/DayBook.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
 import {
     MDBContainer as Container,
     MDBRow as Row,
@@ -10,10 +9,9 @@ import {
 import axios from "axios";
 import { APIS } from "../constants/api";
 import { DaybookService } from '../../services/DaybookService';
-// import AllDaybook from './AllDaybook';
 
 const DayBook = () => {
-    const navigate = useNavigate();
+
     const initialState = {
         id: '',
         date: '',
@@ -23,15 +21,14 @@ const DayBook = () => {
         chequeInFlow: '',
         chequeOutFlow: '',
     }
-   
 
 
     const [formData, setFormData] = useState({ initialState });
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
+            // const response = await axios.post(APIS.CREATEDAYBOOK, formData);
             const response = await DaybookService.createDaybook(formData);
-
             console.log("DayBookId",response.data.id);
 
             if (response.status === 201) {
@@ -49,12 +46,9 @@ const DayBook = () => {
         setFormData((prevData) => ({
             ...prevData,
             [name]: value,
-        }))   
+        }));
+
     };
-    const handleAllDayBook = (event) => {
-        event.preventDefault();
-        navigate("/alldaybook");
-       };
     return (
         <div className=" p-2 mt-5">
             <h1 className=" mb-4 text-center">Day Book </h1>
@@ -133,11 +127,6 @@ const DayBook = () => {
                 <div className="text-center mt-4 ">
                     <Button>Submit</Button>
                 </div>
-                <div className="text-center mt-4 ">
-          <Button variant="primary" type="button" square onClick={handleAllDayBook}>
-          AllDaybook
-          </Button>
-        </div>
             </form>
         </div>
     );
--- src/components/tenant/TenantForm.js
+++ src/components/tenant/TenantForm.js
@@ -1,157 +1,173 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import {
-  MDBContainer as Container,
-  MDBRow as Row,
-  MDBCol as Col,
-  MDBInput as Input,
-  MDBBtn as Button,
-} from "mdb-react-ui-kit";
+    MDBContainer as Container,
+    MDBRow as Row,
+    MDBCol as Col,
+    MDBInput as Input,
+    MDBBtn as Button
+} from 'mdb-react-ui-kit';
 
-import axios from "axios";
-import { APIS } from "../constants/api";
-import { TenantService } from "../../services/TenantService";
+import axios from 'axios';
+import {APIS} from '../constants/api'
+import { TenantService } from '../../services/TenantService';
 
 const TenantForm = () => {
-  const navigate = useNavigate();
-  const initialState = {
-    tenantName: "",
-    address: "",
-    contactNum: "",
-    allocatedShop: "",
-    rentCollected: "",
-    rentDue: "",
-    securityDeposit: "",
-    electricityDue: "",
-    // electricityCollectionDetails:'',
 
-    // date:'',
-  };
+    const initialState ={
+        tenantName: '',
+        address: '',
+        contactNum: '',
+        allocatedShop:'',
+        rentCollected:'',
+        rentDue:'',
+        securityDeposit:'',
+        electricityDue:'',
+        // electricityCollectionDetails:'',
 
-  const [formData, setFormData] = useState(initialState);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await TenantService.createTenant(formData);
-
-      console.log("TenantId", response.data.id);
-
-      if (response.status === 201) {
-        console.log("Tenant Created Successfully");
-        setFormData(initialState);
-      } else {
-        console.error("Failed To create Tenant");
-      }
-    } catch (error) {
-      console.error("Error", error);
+        // date:'',
     }
-    console.log(formData);
-  };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
-  const ShowTenant = (event) => {
-    event.preventDefault();
-    navigate("/showtenant");
-  };
 
-  return (
-    <div className=" p-2 mt-5 ">
-      <h1 className=" mb-4 text-center">Tenant Management</h1>
-      <form onSubmit={handleSubmit}>
-        <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
-          <Col className="col-sm-5 ">
-            <Input
-              label="Name"
-              type="text"
-              name="tenantName"
-              value={formData.tenantName}
-              onChange={handleChange}
-            />
-          </Col>
-          <Col className="col-sm-5 ">
-            <Input
-              label="Contact No"
-              type="tel"
-              name="contactNum"
-              value={formData.contactNum}
-              onChange={handleChange}
-            />
-          </Col>
-        </Row>
-        <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
-          <Col className="col-sm-5 ">
-            <Input
-              label="Current Address"
-              type="text"
-              name="address"
-              value={formData.address}
-              onChange={handleChange}
-            />
-          </Col>
-          <Col className="col-sm-5 ">
-            <Input
-              label="Shop No"
-              type="text"
-              name="allocatedShop"
-              value={formData.allocatedShop}
-              onChange={handleChange}
-            />
-          </Col>
-        </Row>
-        <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
-          <Col className="col-sm-5 ">
-            <Input
-              label="Collected Rent"
-              type="number"
-              name="rentCollected"
-              value={formData.rentCollected}
-              onChange={handleChange}
-            />
-          </Col>
-          <Col className="col-sm-5 ">
-            <Input
-              label="Rent Due"
-              type="text"
-              name="rentDue"
-              value={formData.rentDue}
-              onChange={handleChange}
-            />
-          </Col>
-        </Row>
-        <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
-          <Col className="col-sm-5 ">
-            <Input
-              label="Deposit"
-              type="text"
-              name="securityDeposit"
-              value={formData.securityDeposit}
-              onChange={handleChange}
-            />
-          </Col>
-          <Col className="col-sm-5 ">
-            <Input
-              label="Electricity Due"
-              type="text"
-              name="electricityDue"
-              value={formData.electricityDue}
-              onChange={handleChange}
-            />
-          </Col>
-        </Row>
-        <div className="text-center mt-4 ">
-          <Button type="submit">Submit</Button>
+    const [formData, setFormData] = useState(initialState);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{
+            // const response= await axios.post(APIS.CREATETENANT,formData);
+            const response= await TenantService.createTenant (formData);
+
+            console.log("TenantId",response.data.id);
+
+            if(response.status===201){
+                console.log("Tenant Created Successfully");
+            }else{
+                console.error("Failed To create Tenant");
+            }
+        }catch(error){
+            console.error('Error',error);
+
+        }
+        console.log(formData);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
+
+    
+    return (
+        <div className=" p-2 mt-5 ">
+            <h1 className=" mb-4 text-center">Tenant Management</h1>
+            <form onSubmit={handleSubmit}>
+                <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Name"
+                            type="text"
+                            name="tenantName"
+                            value={formData.tenantName}
+                            onChange={handleChange}
+                        />
+                    </Col>
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Contact No"
+                            type="tel"
+                            name="contactNum"
+                            value={formData.contactNum}
+                            onChange={handleChange}
+                        />
+                    </Col>
+                </Row>
+                <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Current Address"
+                            type="text"
+                            name="address"
+                            value={formData.address}
+                            onChange={handleChange}
+                        />
+                    </Col>
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Shop No"
+                            type="text"
+                            name="allocatedShop"
+                            value={formData.allocatedShop}
+                            onChange={handleChange}
+                        />
+                    </Col>
+                </Row>
+                <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
+                    
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Collected Rent"
+                            type="number"
+                            name="rentCollected"
+                            value={formData.rentCollected}
+                            onChange={handleChange}
+                        />
+                    </Col>
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Rent Due"
+                            type="date"
+                            name="rentDue"
+                            value={formData.rentDue}
+                            onChange={handleChange}
+                        />
+                    </Col>
+                </Row>
+                <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
+                   
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Deposit"
+                            type="text"
+                            name="securityDeposit"
+                            value={formData.securityDeposit}
+                            onChange={handleChange}
+                        />
+                    </Col>
+                    <Col className="col-sm-5 ">
+                        <Input
+                            label="Electricity Due"
+                            type="date"
+                            name="electricityDue"
+                            value={formData.electricityDue}
+                            onChange={handleChange}
+                            
+                        />
+                    </Col>
+                </Row>
+                <Row className="row mt-8 mb-4  justify-content-evenly align-items-center">
+                   
+                    <Col className="col-sm-5 ">
+                        {/* <Input
+                            label="Electricity Collections"
+                            type="text"
+                            // name="electricityCollectionDetails"
+                            // value={formData.electricityCollectionDetails}
+                            onChange={handleChange}
+                            
+                        /> */}
+                         {/* <Input
+                           label=""
+                             type="Date" 
+                             name="date" 
+                             value={formData.date}
+                             onChange={handleChange}
+                        /> */}
+                    </Col>
+                </Row>
+                <div className="text-center mt-4 ">
+                    <Button type="submit">Submit</Button>
+                </div>
+            </form>
         </div>
-        <div className="text-center mt-4 ">
-          <Button variant="primary" type="button" square onClick={ShowTenant}>
-            Show Tenant
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 export default TenantForm;
